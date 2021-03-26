455175c36799008259bafa2d8a000d3cd99b0f1a
Miles <75916236+Miles-oss@users.noreply.github.com>
2021-03-26 19:01:48 -0400

    First

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import hashlib
+import socket
+import time
+import os
+
+class Main:
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        while True:
+            pass
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
