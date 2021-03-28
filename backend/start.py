b97a3d4b075c502eb1ce6614b8a1ff8c850e535d
Miles <75916236+Miles-oss@users.noreply.github.com>
2021-03-28 06:10:59 -0400

    FIXES AGAIN

--- backend/start.py
+++ backend/start.py
@@ -7,8 +7,8 @@ def Start():
     path1 = os.path.join(dir_path, "lib/pypy3.7/pypy3.exe")
     path2 = os.path.join(dir_path, "src/main.py")
     cmd = path1 + ' ' + path2
-    os.system(cmd)
     subprocess.Popen('wallet/WebSock.exe', creationflags=CREATE_NEW_CONSOLE)
+    os.system(cmd)
 
 
 if __name__ == '__main__':
